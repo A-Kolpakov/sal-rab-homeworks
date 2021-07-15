c7a2cc0523cf7a0ac96eae8fba8b9b3108f35e79
Alexey <papa-gin@yandex.ru>
2021-07-15 18:44:56 +0300

    нужна помощь строка 52 и 83

--- task_2/solution.js
+++ task_2/solution.js
@@ -16,7 +16,20 @@ function calcShipping(sum, min, shipping) {
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
+    let shippingSum
+    if (productsSum == 0){
+       shippingSum = 0
+    }
+    if (productsSum >= freeShippingMinSum){
+       shippingSum = 0
+    }
+    if (productsSum > 0 && productsSum < freeShippingMinSum){
+      shippingSum = shippingPrice
+   }
+
     // Конец решения задания №2.1.
+
+
 
     return shippingSum;
 }
@@ -34,6 +47,12 @@ function calcDiscount(sum, min, discount) {
     // то присвойте discountPrice значение discountPart процентов от productsSum,
     // иначе присвойте discountPrice значения 0
 
+    let discountPrice
+    if (productsSum >= discountMinSum){
+        discountPrice = 100/productsSum*discountPart
+    } else {discountPrice=0
+
+    }
     // Конец решения задания №2.2.
 
     return discountSum;
@@ -46,18 +65,22 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
     // создайте переменную totalSum
-
+    let totalSum
     // присвойте totalSum значение productsSum
+    totalSum = productsSum
     // уменьшите totalSum на discountSum
+    totalSum-discountSum
+
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
     // прибавьте к totalSum значение shippingSum
-
+    totalSum+shippingSum
     // создайте переменную freeShipping
+    let freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
-
+    shippingSum == 0
     // Конец решения задачи №2.3.
 
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
